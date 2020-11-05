294d32462b4b8e71e071481e92fdbfc4fb48cade
Samadi van Koten <samadi@vktec.org.uk>
2020-11-05 00:02:17 +0000

    Write simple compiler binary and add example code

--- main.go
+++ main.go
@@ -1,38 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"flag"
+	"io/ioutil"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-	prog, err := Parse(`
-	// Very amazing program to sum two inputted numbers
-	fn puts(str [I8]) I32
-	variadic fn scanf(fmt [I8]) I32
-	variadic fn printf(fmt [I8]) I32
-	pub fn main() I32 {
-		puts("Enter two numbers:")
+	log.SetFlags(0)
 
-		var a, b I32
-		printf("> ")
-		scanf("%d", &a)
-		printf("> ")
-		scanf("%d", &b)
+	out := flag.String("o", "", "output `filename`")
+	as := flag.String("as", "as", "`name` of assembler to use")
+	ld := flag.String("ld", "ld", "`name` of linker to use")
+	verbose := flag.Bool("v", false, "verbose output")
+	flag.Parse()
 
-		var c I32
-		var d [I32]
-		d = &c
-		[d] = 2 * (a + b)
-
-		printf("2 * (%d + %d) = %d\n", a, b, c)
-		return 0
+	if flag.NArg() < 1 {
+		log.Fatal("usage")
 	}
-	`)
 
+	if *out == "" {
+		file := flag.Arg(0)
+		*out = strings.TrimSuffix(file, filepath.Ext(file))
+	}
+
+	tmpDir, err := ioutil.TempDir("", "c4-build-*")
 	if err != nil {
-		fmt.Println("Parse error:", err)
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	var objs []string
+	for _, file := range flag.Args() {
+		if *verbose {
+			log.Print("C4 ", file)
+		}
+
+		data, err := ioutil.ReadFile(file)
+		prog, err := Parse(string(data))
+		if err != nil {
+			log.Fatalln("Parse error:", err)
+		}
+
+		qbeR, qbeW, err := os.Pipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		asR, asW, err := os.Pipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		objFile, err := ioutil.TempFile(tmpDir, "*.o")
+		if err != nil {
+			log.Fatal(err)
+		}
+		objFile.Close() // I wish I could pass the fd directly to as
+
+		qbeCmd := exec.Command("qbe")
+		qbeCmd.Stdin = qbeR
+		qbeCmd.Stdout = asW
+		qbeCmd.Stderr = os.Stderr
+		if err := qbeCmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		asCmd := exec.Command(*as, "-o", objFile.Name(), "-")
+		asCmd.Stdin = asR
+		asCmd.Stdout = objFile
+		asCmd.Stderr = os.Stderr
+		if err := asCmd.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		NewCompiler(qbeW).Compile(prog)
+		qbeW.Close()
+		if err := qbeCmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+		asW.Close()
+		if err := asCmd.Wait(); err != nil {
+			log.Fatal(err)
+		}
+
+		objs = append(objs, objFile.Name())
 	}
 
-	NewCompiler(os.Stdout).Compile(prog)
+	if *verbose {
+		log.Print("LD ", *out)
+	}
+	ldCmd := exec.Command(*ld, append(objs, "-o", *out)...)
+	ldCmd.Stderr = os.Stderr
+	if err := ldCmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
